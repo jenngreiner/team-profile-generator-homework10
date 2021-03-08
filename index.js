e0253bd9b7036f99b1f820da5e6eb0b89df4af34
Jenn Greiner <jenn.greiner1@gmail.com>
2021-03-08 15:06:21 -0700

    html and css code

---
+++ index.js
@@ -0,0 +1,14 @@
+// command line prompts name, job title, ID, Email, Github
+// I am prompted to enter the team manager’s name, employee ID, email address, and office number
+// WHEN I enter the team manager’s name, employee ID, email address, and office number
+// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
+// WHEN I select the engineer option
+// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
+// WHEN I select the intern option
+// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
+// // Different icons for each job type
+
+
+
+// create html page with employee info in cards
+
